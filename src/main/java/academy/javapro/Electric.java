e29a1f3e29802fd5a6a56a821d5418c035622ac1
MEHSOYY <72802928+MEHSOYY@users.noreply.github.com>
2025-02-10 21:58:41 -0500

    Electric.java

--- src/main/java/academy/javapro/Electric.java
+++ src/main/java/academy/javapro/Electric.java
@@ -1,4 +1,12 @@
+/* Project: Tesla Vehicle System
+ * Interface: Electric.java
+ * Author: MEHMET SOYDAN
+ * Date: February 10 2024
+ * This interface defines methods for electric vehicle functionality, such as charging
+ * and checking charging status.
+ */
 package academy.javapro;
-
-public class Electric {
+public interface Electric {
+    void charge();
+    boolean isCharging();
 }
