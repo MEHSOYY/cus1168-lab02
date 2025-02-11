924b2f26b99c9085ab9078b9e4fdaebbb334f0d9
MEHSOYY <72802928+MEHSOYY@users.noreply.github.com>
2025-02-10 22:00:28 -0500

    Tesla.java

--- src/main/java/academy/javapro/Tesla.java
+++ src/main/java/academy/javapro/Tesla.java
@@ -1,13 +1,77 @@
+/* Project: Tesla Vehicle System
+ * Class: Tesla.java
+ * Author: MEHMET SOYDAN
+ * Date: February 10 2024
+ * This class represents a Tesla vehicle, extending the Vehicle class and implementing
+ * the Electric and Autonomous interfaces. It provides concrete implementations for all
+ * abstract and interface methods.
+ */
 package academy.javapro;
-
 public class Tesla extends Vehicle implements Electric, Autonomous {
     private boolean autopilotEnabled;
     private boolean charging;
 
-    // TODO: Create constructor that takes model and year
-    // TODO: Set make to "Tesla"
-    // TODO: Initialize autopilotEnabled and charging to false
+    // ---------------------------------------------------------------
+    // Constructor to initialize Tesla model and year.
+    public Tesla(String model, int year) {
+        super("Tesla", model, year); // Set make to "Tesla"
+        this.autopilotEnabled = false; // Initialize autopilotEnabled to false
+        this.charging = false; // Initialize charging to false
+    }
 
-    // TODO: Implement all required methods from Vehicle, Electric, and Autonomous
-    // Each method should include appropriate print statements
+    // ---------------------------------------------------------------
+    // Implement Vehicle abstract methods
+
+    public void startEngine() {
+        isRunning = true;
+        System.out.println(make + " " + model + " started");
+    }
+
+
+    public void stopEngine() {
+        isRunning = false;
+        System.out.println(make + " " + model + " stopped");
+    }
+
+
+    public void accelerate() {
+        System.out.println(make + " " + model + " is accelerating...");
+    }
+
+
+    public void brake() {
+        System.out.println(make + " " + model + " is braking...");
+    }
+
+    // ---------------------------------------------------------------
+    // Implement Electric interface methods
+
+    public void charge() {
+        charging = true;
+        System.out.println(make + " " + model + " is now charging");
+    }
+
+
+    public boolean isCharging() {
+        return charging;
+    }
+
+    // ---------------------------------------------------------------
+    // Implement Autonomous interface methods
+
+    public void enableAutopilot() {
+        autopilotEnabled = true;
+        System.out.println("Autopilot enabled");
+    }
+
+
+    public void disableAutopilot() {
+        autopilotEnabled = false;
+        System.out.println("Autopilot disabled");
+    }
+
+
+    public boolean isAutopilotEnabled() {
+        return autopilotEnabled;
+    }
 }
