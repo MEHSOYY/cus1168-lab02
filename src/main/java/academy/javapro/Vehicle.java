b8f184785003dd837a92adc2f34688c681b9f439
MEHSOYY <72802928+MEHSOYY@users.noreply.github.com>
2025-02-10 21:57:34 -0500

    Vehicle.java

--- src/main/java/academy/javapro/Vehicle.java
+++ src/main/java/academy/javapro/Vehicle.java
@@ -1,14 +1,63 @@
+/* Project: Tesla Vehicle System
+ * Class: Vehicle.java
+ * Author: MEHMET SOYDAN
+ * Date: February 10 2024
+ * This abstract class represents a generic vehicle with properties like make, model, year,
+ * and running status. It includes abstract methods for vehicle behavior.
+ */
 package academy.javapro;
-
 public abstract class Vehicle {
     protected String make;
     protected String model;
     protected int year;
     protected boolean isRunning;
 
-    // TODO: Add constructor
+    // ---------------------------------------------------------------
+    // Constructor to initialize make, model, and year.
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.isRunning = false; // Initialize isRunning to false
+    }
 
-    // TODO: Add abstract methods
+    // Abstract methods to be implemented by subclasses
+    public abstract void startEngine();
+    public abstract void stopEngine();
+    public abstract void accelerate();
+    public abstract void brake();
 
-    // TODO: Add getters/setters
+    // ---------------------------------------------------------------
+    // Getter and setter methods for all fields
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public void setRunning(boolean running) {
+        isRunning = running;
+    }
 }
