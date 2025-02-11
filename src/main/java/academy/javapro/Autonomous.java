233e08888c3bee9597d15dd0d679b44b29011e2f
MEHSOYY <72802928+MEHSOYY@users.noreply.github.com>
2025-02-10 21:59:14 -0500

    Update Autonomous.java

--- src/main/java/academy/javapro/Autonomous.java
+++ src/main/java/academy/javapro/Autonomous.java
@@ -1,9 +1,13 @@
+/* Project: Tesla Vehicle System
+ * Interface: Autonomous.java
+ * Author: MEHMET SOYDAN
+ * Date: February 10 2024
+ * This interface defines methods for autonomous vehicle functionality, such as enabling
+ * or disabling autopilot and checking its status.
+ */
 package academy.javapro;
-
 public interface Autonomous {
     void enableAutopilot();
-
     void disableAutopilot();
-
     boolean isAutopilotEnabled();
 }
